62f14d7048566f98dda7722a091aa6aad14454db
NickMattmiller <njmatt92@gmail.com>
2015-09-01 20:32:01 -0400

    Step two working
    Commiting before adding third activity

--- app/src/main/java/com/example/nick/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/nick/myfirstapp/DisplayMessageActivity.java
@@ -5,34 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
+public class DisplayMessageActivity extends AppCompatActivity {
 
-public class MyActivity extends AppCompatActivity {
-
-
-    public final static String EXTRA_MESSAGE = "com.example.nick.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
